e964c9cd623deca2bec90662f614fa6c8c80c700
Enzzx <101784007+Enzzx@users.noreply.github.com>
2024-05-27 15:07:27 -0300

    finalizado

--- sketch.js
+++ sketch.js
@@ -2,120 +2,141 @@ const vectors = 40;
 const extraVectors = 2
 let xDrop, yDrop
 let pressed = false
+let message = true
 
-let slider, check1, check2;
+let slider, check1, check2
 let arrow;
-const matrix = [];
-let charges = [];
+const matrix = []
+let charges = []
 
 function preload() {
-  arrow = loadImage("arrow.png");
+  arrow = loadImage("arrow.png")
 }
 
 function setup() {
-  frameRate(15);
-  const canvas = createCanvas(700, 700);
+  frameRate(15)
+  const canvas = createCanvas(700, 700)
   angleMode(DEGREES)
 
-  //gerar grid do campo elétrico
-  const vectorW = int(width / vectors);
-  const vectorH = int(height / vectors);
+  //gerar grid de vetores
+  const vectorW = int(width / vectors)
+  const vectorH = int(height / vectors)
 
   yDrop = -vectorH * extraVectors
 
   for (let i = 0; i < vectors + 2 * extraVectors; i++) {
-    const rows = [];
-    matrix.push(rows);
+    const rows = []
+    matrix.push(rows)
 
-    xDrop = -vectorW * extraVectors;
+    xDrop = -vectorW * extraVectors
     for (let j = 0; j < vectors + 2 * extraVectors; j++) {
-      rows.push(new vector(xDrop, yDrop));
-      xDrop += vectorW;
+      rows.push(new vector(xDrop, yDrop))
+      xDrop += vectorW
     }
-    yDrop += vectorH;
+    yDrop += vectorH
   }
 
 
+  //limpar tela
   const trash = createButton("Apagar")
   trash.position(width - 80, height + 20)
-  trash.mousePressed(()=> charges.length = 0)
+  trash.mousePressed(() => {
+    charges.length = 0
+    message = false
+  })
+  
   // valores de carga
-  slider = createSlider(50, 125, 75);
-  check1 = createCheckbox("Positiva", true);
-  check1.changed(checkVerification);
-  check2 = createCheckbox("Negativa", false);
-  check2.changed(checkVerification);
+  slider = createSlider(50, 125, 75)
+  check1 = createCheckbox("Positiva", true)
+  check1.changed(checkVerification)
+  check2 = createCheckbox("Negativa", false)
+  check2.changed(checkVerification)
+  
   //criar carga elétrica
   canvas.mouseClicked(() => {
     if (charges.length >= 7 || pressed) {
       pressed = false
       return;
     }
-    charges.push(new charge(mouseX, mouseY, chargeMass, cation));
+    charges.push(new charge(mouseX, mouseY, chargeMass, cation))
   });
 }
 
 function draw() {
-  background(220);
-  fill("black")
-  chargeMass = slider.value();
-  cation = check1.checked();
-  anion = check2.checked();
-  textSize(20)
-  fill("purple")
-  text(`Massa: ${chargeMass}  carga: ${cation ? "Positiva" : "Negativa"}`, 0, height - 5);
+  if (message) {
+    background(30)
+    fill(255)
+    textSize(30)
+    textAlign(CENTER)
+    text(`Pressione para posicionar uma carga\n Arraste para simular uma carga`, width/2, height/2)
+    textSize(18)
+    text("Aperte 'Apagar' para limpar a tela", width/2, height - 100)
+  } else {
+    background(220)
+    fill("black")
+    chargeMass = slider.value()
+    cation = check1.checked()
+    anion = check2.checked()
+    textSize(20)
+    textAlign(LEFT)
+    fill("purple")
+    text(`Massa: ${chargeMass}  carga: ${cation ? "Positiva" : "Negativa"}`, 0, height - 5)
 
-  matrix.forEach((rows) => {
-    rows.forEach((vector) => {
-      vector.direction();
-      vector.magnitude();
+    matrix.forEach((rows) => {
+      rows.forEach((vector) => {
+        vector.direction()
+        vector.magnitude()
+      });
     });
-  });
 
-  charges.forEach((charge) => {
-    charge.position();
-  });
+    charges.forEach((charge) => {
+      charge.position()
+    });
+  }
 }
 
 function mousePressed() {
+  if (mouseX > width || mouseY > height) { return }
   charges.push(new charge(mouseX, mouseY, chargeMass, cation))
 }
 function mouseDragged() {
+  if (mouseX > width || mouseY > height) { return }
   pressed = true
   charges[charges.length-1].x = mouseX
   charges[charges.length-1].y = mouseY
 }
 function mouseReleased() {
+  if (mouseX > width || mouseY > height) { return }
   charges.pop()
 }
 
 class charge {
   constructor(x, y, mass, positive) {
-    this.x = x;
-    this.y = y;
-    this.mass = mass;
-    this.positive = positive;
-    this.color = positive ? "#D83838" : "#4242EE";
+    this.x = x
+    this.y = y
+    this.mass = mass
+    this.positive = positive
+    this.color = positive ? "#D83838" : "#4242EE"
   }
 
   position() {
-    noStroke();
-    fill(this.color);
-    circle(this.x, this.y, map(this.mass, 50, 125, 20, 40));
+    noStroke()
+    fill(this.color)
+    circle(this.x, this.y, map(this.mass, 50, 125, 20, 40))
   }
 }
 
 
 class vector {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.force = 0;
+    this.x = x
+    this.y = y
+    this.force = 0
     this.graus = 0
   }
 
   direction() {
-    //calcular a direção e força da interação
+    //calcular a direção e força da interação elétrica
     if(charges.length == 0) {
       this.force = 0
       this.graus = 0
@@ -141,7 +162,7 @@ class vector {
       yTotal += E * sin(this.graus)
     })
                              
-    this.force = sqrt(xTotal**2 + yTotal**2) * 2
+    this.force = sqrt(xTotal**2 + yTotal**2) * 2 //* 2 apenas para aumentar a escala dos vetores
     this.graus = atan2(yTotal, xTotal)
     
     this.force = this.force > 100 ? 100 : this.force
@@ -149,13 +170,14 @@ class vector {
 
   magnitude() {
     //mudar o tamanho e transparencia de acordo com a magnitude da força
-    
     push()
     translate(this.x, this.y)
     rotate(this.graus)
     //tint(255, 0, 0, 100)
     scale(map(this.force, 0, 100, 0.8, 1.8))
-    image(arrow, 0, 0, width / (vectors * 1.5), height / (vectors * 1.5));
+    image(arrow, 0, 0, width / (vectors * 1.5), height / (vectors * 1.5))
+    //stroke(0)
+    //line(0, 0, map(this.force, 0, 100, 0, 20), 0)
     pop()
   }
 }
@@ -166,11 +188,11 @@ function checkVerification() {
     (!check1.checked() && !check2.checked())
   ) {
     if (this == check1) {
-      check1.checked(true);
-      check2.checked(false);
+      check1.checked(true)
+      check2.checked(false)
     } else {
-      check1.checked(false);
-      check2.checked(true);
+      check1.checked(false)
+      check2.checked(true)
     }
   }
 }
